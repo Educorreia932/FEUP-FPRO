93fbeb2b00e0d3eead460b4a7a815abb961a1797
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-01-06 16:45:27 +0000

    Add files via upload

--- 12. Files/diff.py
+++ 12. Files/diff.py
@@ -1,25 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  1 14:47:03 2019
+Created on Sat Jan  5 20:44:40 2019
 
 @author: Asus
 """
-
+ 
 def diff(filename1, filename2):
     with open(filename1, "r") as file1:
         with open(filename2, "r") as file2:
+            minus = ""
+            plus = ""
+            index = 0            
             lines1 = file1.readlines()
             lines2 = file2.readlines()
-            result = ""
-            
-            for line in lines1:
-                if line not in lines2:
-                    result += "- " + line 
             
             for line in lines2:
-                if line not in lines1:
-                    result += "+ " + line
-                     
-            return result[:-1]
+                if index > len(lines1) - 1:
+                    plus += "+ " + lines2[index + 1]
+                                        
+                    return minus + plus
+                
+                else:               
+                    if index != 0 and line == lines1[index] :
+                        return (minus + plus)[:-1]
+                    
+                    elif line != lines1[index]:
+                        this_line = lines1[index]
+                        lines1.remove(lines1[index])
+                        
+                        if index > len(lines1) - 1:
+                            minus += "- " + this_line + "\n"
+                            plus += "+ " + lines2[index]
+                            
+                            return (minus + plus)[:-1]
+                        
+                        elif line == lines1[index]:
+                            minus += "- " + this_line
+                            
+                            return (minus + plus)[:-1]
+                        
+                                                    
+                        else:      
+                            lines1.insert(index, line)
+                            minus += "- " + this_line
+                            plus += "+ "+ line
+                    
+                index += 1
         
 print(diff("file1d.txt", "file2d.txt"))
--- 12. Files/grep.py
+++ 12. Files/grep.py
@@ -6,115 +6,76 @@ Created on Sat Jan  5 13:53:08 2019
 """
 
 def grep(pattern, files_list, flags=''):
-    with open(files_list[0], "r") as file1:
-        with open(files_list[1], "r") as file2:
-            result = ""
-            lines1 = file1.readlines()
-            lines2 = file2.readlines()
-            lines1_copy = lines1.copy()
-            lines2_copy = lines2.copy()
-            index1 = 0
-            index2 = 0
+    result = ""
+    
+    for file in files_list:
+        with open(file, "r") as this_file:            
+            lines = this_file.readlines()
             
-            if "-i" in flags:
-                for line in lines1:
-                    lines1[lines1.index(line)] = line.lower()
+        lines_copy = lines.copy()
+        index = 0
+        
+        if "-i" in flags:
+            for line in lines:
+                lines[index] = line.lower()
+                index += 1
+           
+            pattern = pattern.lower()
+         
+        index = 0    
+            
+        if flags == "-i":
+            for line in lines:
+                if pattern in line[: -1]:
+                    result +=  file + ":" + str(index + 1) + ":" + lines_copy[index]
+      
+                index += 1
+   
+        index = 0
+           
+        if "-x" in flags:   
+            for line in lines:
+                if pattern == line[: -1]:
+                    result +=  file + ":" + str(index + 1) + ":" + lines_copy[index]
+      
+                index += 1
+        
+        index = 0
+        
+        if "-v" in flags:
+            for line in lines:
+                if pattern not in line:
+                    result +=  file + ":" + str(index + 1) + ":" + lines_copy[index]
+      
+                index += 1
+              
+            result += "\n"   
+                
+        index = 0
+        
+        if "-l" in flags:
+            for line in lines:
+                if pattern in line:
+                    result += file
+                    break
+        
+            result += "\n" 
+        
+        
+        if "-c" in flags:    
+            count = 0
+            
+            for line in lines:
+                if pattern in line:                        
+                    count += 1   
                     
-                for line in lines2:
-                    lines2[lines2.index(line)] = line.lower()   
-               
-                pattern = pattern.lower()
-                
-            if flags == "-i":
-                for line in lines1:
-                    if pattern in line[: -1]:
-                        result +=  files_list[0] + ":" + str(index1 + 1) + ":" + lines1_copy[index1]
+            result += file + ":" + str(count) + "\n"  
+        
+        if flags == "":
+            for line in lines:
+                if pattern in line:
+                    result +=  file + ":" + str(lines.index(line) + 1) + ":" + line
           
-                    index1 += 1
-            
-                for line in lines2:
-                    if pattern in line[: -1]:
-                        result +=  files_list[1] + ":" + str(index2 + 1) + ":" + lines2_copy[index2]
-            
-                    index2 += 1
-            
-            index1 = 0
-            index2 = 0
-                
-            if "-x" in flags:   
-                for line in lines1:
-                    if pattern == line[: -1]:
-                        result +=  files_list[0] + ":" + str(index1 + 1) + ":" + lines1_copy[index1]
-          
-                    index1 += 1
-            
-                for line in lines2:
-                    if pattern == line[: -1]:
-                        result +=  files_list[1] + ":" + str(index2 + 1) + ":" + lines2_copy[index2]
-            
-                    index2 += 1
-            
-            index1 = 0
-            index2 = 0
-            
-            if "-v" in flags:
-                for line in lines1:
-                    if pattern not in line:
-                        result +=  files_list[0] + ":" + str(index1 + 1) + ":" + lines1_copy[index1]
-          
-                    index1 += 1
-                  
-                result += "\n"   
-                    
-                for line in lines2:
-                    if pattern not in line:
-                        result +=  files_list[1] + ":" + str(index2 + 1) + ":" + lines2_copy[index2]
-            
-                    index2 += 1
-            
-                result += "\n"   
-                
-            index1 = 0
-            index2 = 0
-            
-            if "-l" in flags:
-                for line in lines1:
-                    if pattern in line:
-                        result +=  files_list[0]
-                        break
-            
-                result += "\n" 
-            
-                for line in lines2:
-                    if pattern in line:
-                        result +=  files_list[1]
-                        break
-                         
-                result += "\n" 
-            
-            if "-c" in flags:    
-                count1 = 0
-                count2 = 0
-                
-                for line in lines1:
-                    if pattern in line:                        
-                        count1 += 1   
-            
-                for line in lines2:
-                    if pattern in line:
-                        count2 += 1
-                        
-                result += files_list[0] + ":" + str(count1) + "\n" + files_list[1] + ":" + str(count2) + "\n"      
-            
-            if flags == "":
-                for line in lines1:
-                    if pattern in line:
-                        result +=  files_list[0] + ":" + str(lines1.index(line) + 1) + ":" + line
-          
-                for line in lines2:
-                    if pattern in line:
-                        result +=  files_list[1] + ":" + str(lines2.index(line) + 1) + ":" + line
-            
-            return result[:-1]
+    return result[:-1]
         
 print(grep('Hello', ['file1g.txt', 'file2g.txt'], ''))
